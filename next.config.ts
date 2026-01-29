447c7d0b2cd68817bc6ef1aeada0d9fdfb6c0e2d
JOSEP <transportscatsud@gmail.com>
2026-01-29 09:48:03 +0100

    mejoras en ios

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,13 @@ import withPWAInit from "@ducanh2912/next-pwa";
 const withPWA = withPWAInit({
   dest: "public",
   register: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
   disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    skipWaiting: true,
+  },
 });
 
 const nextConfig: NextConfig = {
